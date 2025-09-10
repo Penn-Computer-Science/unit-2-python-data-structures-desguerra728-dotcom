6b4b51b3aa181f820e5b682f4eb7157f2abdfefc
desguerra728 <desguerra728@phm.k12.in.us>
2025-09-10 09:51:12 -0400

    Update gradebookManager.py

--- gradebookManager.py
+++ gradebookManager.py
@@ -17,7 +17,7 @@ def add_grade():
     while True:
         try:
             grade = input("Enter grade: ")
-            float(grade)
+            grade = float(grade)
             break
         except:
             print("Please enter a number.")
@@ -33,10 +33,6 @@ gradebook = {"Billy":[76, 89, 83],
 print(gradebook)
 
 
-add_student()
-add_grade()
-
-
 # 2. User Input
 #   - View the gradebook summary
 
@@ -46,14 +42,56 @@ add_grade()
 
 # 4. Display
 # - Print a clear summary of all students, their grades, and their average.
-# - Example output:
-#   - Alice: [90, 85, 92] Average: 89.0
-#   - Bob: [78, 81, 86] Average: 81.7
-#   - Top Student: Alice
+
+def calculate_avg(student):
+    sum = 0
+    grades = gradebook[student]
+    for grade in grades:
+        sum += grade
+    average = sum/len(grades)
+    return average
+
+def summary():
+    for student in gradebook:
+        avg = calculate_avg(student)
+        print(student + ": " + gradebook[student] + " Average : " + str(avg))
+    #   - Alice: [90, 85, 92] Average: 89.0
+    #   - Bob: [78, 81, 86] Average: 81.7
+    #   - Top Student: Alice
+    pass
+
 
 # ---
 # 5. Additional Features (Required)
 # - Allow the user to remove a student or a grade.
+
+def remove_student():
+    pass
+
+def remove_grade():
+    pass
+
 # - Display letter grades (A, B, C, etc.) based on averages.
+def view_letter():
+    pass
+
 # - Sort students by average grade.
+def sort():
+    pass
+
 # ---
+'''
+print("Welcome to the gradebook.")
+while True:
+    print("")
+    use = input("1: Add new student\n2: Add grade\n3: View gradebook summary\nX: Close gradebook\nEnter choice: ")
+    print("")
+    if use == "1":
+        add_student()
+    if use == "2":
+        add_grade()
+    if use == "3":
+        print("ND")
+    if use == "4":
+        break
+'''
